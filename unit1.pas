74aee9e1381ac1f80cf345960a3e253be0257325
Hevanafa <dhevansafa4@gmail.com>
2026-06-24 10:08:00 +0700

    First upload

---
+++ unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+{$Mode ObjFPC}
+{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  Forms, Controls, Graphics, Dialogs, StdCtrls, SynHighlighterPas, SynEdit;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    SnippetList: TListBox;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
