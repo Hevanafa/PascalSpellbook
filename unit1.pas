9f51928233f1422177247e1b31d31bb638f5c93f
Hevanafa <dhevansafa4@gmail.com>
2026-06-24 10:18:47 +0700

    added list all snippets

--- unit1.pas
+++ unit1.pas
@@ -15,6 +15,7 @@ type
 
   TForm1 = class(TForm)
     SnippetList: TListBox;
+    procedure FormShow(Sender: TObject);
   private
 
   public
@@ -27,6 +28,27 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  searchRec: TSearchRec;
+  targetDir: string;
+begin
+  targetDir := '.\snippets\';
+
+  if FindFirst(targetDir + '*.pas', faAnyFile, searchRec) = 0 then begin
+    repeat
+      if (searchRec.Name <> '.') and (searchRec.Name <> '..') then begin
+        if (searchRec.Attr and faDirectory) = 0 then
+          SnippetList.AddItem(searchRec.name, nil);
+      end;
+    until FindNext(searchRec) <> 0;
+
+    FindClose(searchRec)
+  end;
+end;
 
 end.
 
