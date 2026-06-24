cb2d11403b6636d23debe28ebaaef3d84505c1fb
Hevanafa <dhevansafa4@gmail.com>
2026-06-24 10:39:29 +0700

    added basic metadata loader

--- unit1.pas
+++ unit1.pas
@@ -6,17 +6,38 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils,
+  Classes, SysUtils, FGL,
   Forms, Controls, Graphics, Dialogs, StdCtrls, SynHighlighterPas, SynEdit;
 
 type
+
+  { TMetadata }
+
+  TMetadata = class
+  public
+    Title: string;
+    Author: string;
+    Description: string;
+    Category: string;
+    Requires: string;
+    Tags: string;
+    Website: string;
+    Licence: string;
+    Version: string;
+
+    constructor FromCode(const code: string);
+  end;
+
+  TMetadataList = specialize TFPGObjectList<TMetadata>;
 
   { TForm1 }
 
   TForm1 = class(TForm)
     SnippetList: TListBox;
     procedure FormShow(Sender: TObject);
+
   private
+    metadataList: TMetadataList;
 
   public
 
@@ -29,20 +50,60 @@ implementation
 
 {$R *.lfm}
 
+
+const
+  SnippetsDir = '.\snippets\';
+
+{ TMetadata }
+
+constructor TMetadata.FromCode(const code: string);
+var
+  lines: TStringArray;
+  line, trimmed: string;
+  beginMetadata: boolean;
+begin
+  lines := code.Split(LineEnding);
+
+  for line in lines do begin
+    trimmed := Trim(line);
+
+    if trimmed = '{' then beginMetadata := true;
+    if trimmed = '}' then beginMetadata := false;
+
+    if not beginMetadata then continue;
+
+    if trimmed.ToLower.Contains('title:') then
+      self.Title := trim(copy(trimmed, 7));
+  end;
+end;
+
 { TForm1 }
 
 procedure TForm1.FormShow(Sender: TObject);
 var
   searchRec: TSearchRec;
   targetDir: string;
+  filename: string;
+  reader: TStringList;
+  metadata: TMetadata;
 begin
-  targetDir := '.\snippets\';
+  targetDir := SnippetsDir;
+
+  metadataList := TMetadataList.create;
+  reader := TStringList.create;
 
   if FindFirst(targetDir + '*.pas', faAnyFile, searchRec) = 0 then begin
     repeat
       if (searchRec.Name <> '.') and (searchRec.Name <> '..') then begin
-        if (searchRec.Attr and faDirectory) = 0 then
-          SnippetList.AddItem(searchRec.name, nil);
+        if (searchRec.Attr and faDirectory) = 0 then begin
+          filename := searchRec.name;
+          reader.LoadFromFile(SnippetsDir + filename);
+          metadata := TMetadata.FromCode(reader.text);
+
+          SnippetList.AddItem(metadata.title, nil);
+
+          reader.free
+        end;
       end;
     until FindNext(searchRec) <> 0;
 
