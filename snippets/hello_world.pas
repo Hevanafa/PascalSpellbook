8502bd93c183db73248bae6f58be6a51e67be6ca
Hevanafa <dhevansafa4@gmail.com>
2026-06-24 10:14:08 +0700

    added metadata example

---
+++ snippets/hello_world.pas
@@ -0,0 +1,15 @@
+{
+  Title: Hello world
+  Author: Hevanafa
+  Description: A hello world example in Free Pascal
+  Category: Basics
+  Requires: (none)
+  Tags: beginner, output
+  Website: https://github.com/Hevanafa/PascalSpellbook
+  Licence: MIT
+  Version: 0.1
+}
+
+begin
+  write('Hello world!')
+end.
